a3c7a19f8355317095a1804be72b0aac4eb10582
ElsioAyala <elsio_racing@hotmail.com>
2022-06-29 13:54:03 -0300

    componentes header y banner

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     CommonModule,
